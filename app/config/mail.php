52062d34dd1ffc02e424e20fcf65547a3280eb57
trunghieule <letrunghieu.cse09@gmail.com>
2013-07-04 16:56:59 +0700

    Add packages and edit git ignore

--- app/config/mail.php
+++ app/config/mail.php
@@ -41,7 +41,7 @@ return array(
 	|
 	*/
 
-	'port' => 587,
+	'port' => 465,
 
 	/*
 	|--------------------------------------------------------------------------
@@ -54,7 +54,7 @@ return array(
 	|
 	*/
 
-	'from' => array('address' => null, 'name' => null),
+	'from' => array('address' => 'noreply@calos.mailgun.org', 'name' => 'CALOS Service'),
 
 	/*
 	|--------------------------------------------------------------------------
@@ -80,7 +80,7 @@ return array(
 	|
 	*/
 
-	'username' => null,
+	'username' => 'postmaster@calos.mailgun.org',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -93,7 +93,7 @@ return array(
 	|
 	*/
 
-	'password' => null,
+	'password' => '261cbl3vqsp5',
 
 	/*
 	|--------------------------------------------------------------------------
